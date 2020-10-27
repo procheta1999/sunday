72622c1e431489522801e4628b215a08d36e2295
procheta1999 <bhattacharyya.procheta1999@gmail.com>
2020-10-28 03:31:20 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,21 @@ import { trackPromise } from 'react-promise-tracker';
 import { Route, Switch } from 'react-router-dom';
 import Intro from './pages/intro';
 import LoadingOverlay from 'react-loading-overlay';
-
+import Loader from 'react-loader-spinner'
 
 function App() {
 
   return (
     <div className="App">
-<LoadingOverlay
-  active
-  spinner
-
-  text='Loading your content...'
-  >
+      <Loader
+      type="BallTriangle"
+      color="white"
+      timeout={7000}
+      />
       <Switch>
     
         <Route exact path="/" component={Intro}/>
       </Switch>
-      </LoadingOverlay>
     </div>
   );
 }
